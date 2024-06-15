5cd6b993e6460a523c23b259b79084bdc7dab13a
Amina <145813752+Binevochka@users.noreply.github.com>
2024-06-15 15:01:03 +0300

    Delete 6-driver directory

--- 6-driver/hw_3.js
+++
@@ -1,6 +0,0 @@
-const hasLicence = true;
-const age = 19;
-const isDrunk = false;
-
-const canDrive = hasLicence && age > 18 && !isDrunk;
-console.log(`Может водить машину? ${canDrive ? 'Может' : 'Не может'}`);
